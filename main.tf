cc91a1318cfec22a0815f8adf67d7f43aa8e2258
Barney Parker <barney.parker@claranet.uk>
2020-04-11 20:26:03 +0100

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,94 @@
+resource "aws_api_gateway_method" "method" {
+  rest_api_id        = var.api_id
+  resource_id        = var.resource_id
+  http_method        = var.http_method
+  authorization      = var.authorization
+  request_parameters = var.request_parameters
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = var.api_id
+  resource_id             = var.resource_id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:eu-west-1:events:action/PutEvents"
+  credentials             = aws_iam_role.eventbridge_put.arn
+  request_parameters = {
+    "integration.request.header.X-Amz-Target" = "'AWSEvents.PutEvents'"
+    "integration.request.header.Content-Type" = "'application/x-amz-json-1.1'"
+  }
+
+  request_templates = {
+    "application/json" = <<-EOT
+      {
+        "Entries": [
+          {
+            "EventBusName":"${var.bus_name}",
+            "Source":"${var.event_source}",
+            "DetailType":"${var.event_detail_type}",
+            "Detail": "${replace(replace(jsonencode(var.event_detail), "\"", "\\\""), "\n", "")}",
+            "Resources": [],
+            "Time": $context.requestTimeEpoch
+          }
+        ]
+      }
+    EOT
+  }
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+  count = length(var.responses)
+
+  rest_api_id        = var.api_id
+  resource_id        = var.resource_id
+  http_method        = aws_api_gateway_method.method.http_method
+  status_code        = var.responses[count.index].status_code
+  selection_pattern  = var.responses[count.index].selection_pattern
+  response_templates = var.responses[count.index].templates
+}
+
+resource "aws_api_gateway_method_response" "method_response" {
+  count = length(var.responses)
+
+  rest_api_id = var.api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = var.responses[count.index].status_code
+}
+
+resource "aws_iam_role" "eventbridge_put" {
+  name = "${var.name}-eventbridge-put"
+  assume_role_policy = data.aws_iam_policy_document.apigw.json
+}
+
+data "aws_iam_policy_document" "apigw" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "apigateway.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "eventbridge_put" {
+  name = "Event-Bus-Put-Item"
+  role = aws_iam_role.eventbridge_put.id
+  policy = data.aws_iam_policy_document.eventbridge_put.json
+}
+
+data "aws_iam_policy_document" "eventbridge_put" {
+  statement {
+     actions = [
+      "events:*",
+    ]
+
+    resources = [
+      "${var.bus_arn}",
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "name" {}
+
+variable "api_id" {}
+
+variable "resource_id" {}
+
+variable "http_method" {}
+
+variable "bus_arn" {}
+
+variable "authorization" {
+  default     = "NONE"
+}
+
+variable "request_parameters" {
+  type        = map
+  default     = {}
+}
+
+
+#variable "request_templates" {
+#  type        = map
+#  default     = {}
+#}
+
+variable "bus_name" {
+}
+
+variable "event_source" {
+  type        = string
+}
+
+variable "event_detail_type" {
+  type        = string
+}
+
+variable "event_detail" {
+  type        = map
+}
+
+variable "responses" {
+  type        = list
+  default     = []
+}
